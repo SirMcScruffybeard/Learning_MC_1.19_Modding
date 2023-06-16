35edda7774b03f35b6fb1afe1a3ef84728656c4a
SirMcScruffybeard <scruffybeardworx@gmail.com>
2023-06-16 00:22:10 -0400

    Tutorial 2 Items part way through

--- src/main/java/net/scruffy/tutorialmod/TutorialMod.java
+++ src/main/java/net/scruffy/tutorialmod/TutorialMod.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.scruffy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -20,6 +21,8 @@ public class TutorialMod {
     private static final Logger LOGGER = LogUtils.getLogger();
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
